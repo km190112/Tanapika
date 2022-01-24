314e35b0d681cc51f2d2809eaadd3eb8cf96e4a4
Ken Matsuoka <46617422+km190112@users.noreply.github.com>
2022-01-25 05:21:45 +0900

    Add files via upload

--- ESPNOW_Send/ESPNOW_Send.ino
+++ ESPNOW_Send/ESPNOW_Send.ino
@@ -1,22 +1,23 @@
 /**************************************
   PCからシリアル通信で受信してESP-NOWで指定のマイコンにLEDテープの制御データを送信する。　　　
  **************************************
-
+  デバイスはM5StickC
   ESP-NOWはWPA2標準で使用するための標準暗号化プロトコル
   ESP32の開発会社であるEspressifが開発した通信方式で、
   内容的にはIEEE 802.11のCCM Protocolを使って通信
   esp_now_add_peer() 送信先登録：登録最大数は20台
   esp_now_send()     送信：送信先MACアドレスを指定して、250バイトまでのデータを送信可能
-
+  ボーレート UART0(USB):115200bps
+  デフォルトは8bit、パリティなし、1ストップビット
+  
   PCからの送信データイメージ
   MACアドレス,マイコンのフラグ,利用するLEDの間隔,接続しているLEDの最大数,LEDごとの色フラグ\r\n
-  "FF:FF:FF:FF:FF:FF,2,3,150,BNNBBRRRGGGBB\r\n"
+  "00:00:00:00:00:01,2,3,150,BNNBBRRRGGGBB\r\n"
 */
 
+#include <M5StickC.h>
 #include <WiFi.h>
 #include <esp_now.h>
-#include <string.h>
-#include <stdlib.h>
 
 const uint8_t ESPNOW_CHANNEL = 0;  //ESP-NOWのチャンネル番号受信側のチャンネルと合わせる
 // uint8_t PeerAddr[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }; // 受信機のMACアドレスに書き換えます
@@ -31,24 +32,25 @@ esp_now_peer_num_t peerNum;
 // データを受信するための構造
 // 送信者の構造と一致する必要があります。ESP-NOWのペイロード長：205byte/最大250byte
 typedef struct struct_message {
-  int resetF;     // 1=マイコンリセット、2=LEDを点灯、3=LED全消灯
-  int LedStep;    // 利用するLEDの間隔。ledColorが5番目でLedStepが3の場合：16番目のLEDが点灯する
-  int ledLen;     // 接続しているLEDの最大数。ledColorに格納したバイト数
-  char ledColor[];  // LEDごとに先頭から光らせる色を指定(R,G,B,Nのいずれかを指定)最大200番目まで設定可
+  uint8_t resetF;     // 1=マイコンリセット、2=LEDを点灯、3=LED全消灯
+  uint8_t brightness; // LEDの明るさ(20~最大255)
+  uint16_t ledLen;    // 接続しているLEDの最大数。ledColorに格納したバイト数
+  uint16_t LedStep;   // 利用するLEDの間隔。ledColorが5番目でLedStepが3の場合：16番目のLEDが点灯する
+  String ledColor;    // LEDごとに先頭から光らせる色を指定(R,G,B,N,Wのいずれかを指定)最大200番目まで設定可
 } struct_message;
-
-struct_message myData;  // myDataというstruct_messageを作成
+struct_message myData; // myDataというstruct_messageを作成
 
 //PCから受信するときのバッファ
-#define BPS0 115200      // bps UART0(USB)
-#define DATASIZE 300     // 一度に送る文字列のバイト数
-char baffArr[DATASIZE];  // バッファ
-int buffp = 0;
-bool sendF = false;  //False:データ未着、True：送信可
+#define DATASIZE 300   // 一度に送る文字列のバイト数
+char buffArr[DATASIZE];
+uint16_t bufp = 0;
+
+uint8_t PeerAddress[6]; //[0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+string strData = "";
 
 //エラー情報
-int EspNowErrF(esp_err_t *result) {
-  switch (*result) {
+int EspNowErrF(const esp_err_t &flg) {
+  switch (flg) {
     case ESP_OK:
       Serial.println("成功");
       return 0;
@@ -79,23 +81,7 @@ int EspNowErrF(esp_err_t *result) {
 
 
 //ESP-NOWでデータ送信
-int EspNowSend(char *baffArr) {
-  if( baffArr == nullptr ){
-    Serial.println("Null");
-    return;
-  }
-  // 受信機のMACアドレスを配列に変換 //char mac[] = "00:0c:29:56:44:34";
-  uint8_t PeerAddress[6];
-  char temp[3];
-  uint8_t i;
-  for (i = 0; i < 6; i++) {
-    temp[0] = baffArr[i * 3 + 0];
-    temp[1] = baffArr[i * 3 + 1];
-    temp[2] = 0x00;
-    PeerAddress[i] = strtol(temp, NULL, 16);
-    Serial.printf("PeerAddr[%d] = %d\n", i, PeerAddress[i]);
-  }
-  Serial.println("");
+int EspNowSend(const uint8_t &PeerAddress[6] , const string &strData) {
 
   //文字列分割とデータ格納
   char *token; /* 分離後の文字列を指すポインタ */
@@ -125,14 +111,17 @@ int EspNowSend(char *baffArr) {
     // ピアツーピアリストを追加
     memcpy(peerInfo.peer_addr, PeerAddress, 6);
     result = esp_now_add_peer(&peerInfo);
-    if (EspNowErrorF(result) > 0) { return; }
+    if (EspNowErrorF(result) > 0) 
+    {
+      return;
+    }
   }
 
   //ESP-NOWでデータ送信.引数はピアMACアドレス,送信するデータ,データの長さ
   result = esp_now_send(PeerAddr, (uint8_t *)&myData, sizeof(myData));
-  if (EspNowErrorF() > 0)
+  if (EspNowErrorF(result) > 0)
   {
-    return;
+    //何もしない
   }
 
   // ピアが最大数に達している場合は削除
@@ -141,6 +130,10 @@ int EspNowSend(char *baffArr) {
   {
     //ピアリスト削除
     result = esp_now_del_peer(&peerInfo);
+    if (EspNowErrorF(result) > 0)
+    {
+        //何もしない
+    }
   }
 }
 
@@ -161,8 +154,9 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 *********************************************/
 void setup() {
   delay(200);
-  Serial.begin(BPS0);  // UART0
-
+  M5.begin();
+  strData.reserve(DATASIZE); // strDATAの文字列の格納領域をbyte数確保
+  
   delay(50);
   //ESP-NOWの接続
   WiFi.mode(WIFI_STA);
@@ -176,53 +170,80 @@ void setup() {
   esp_now_register_send_cb(OnDataSent);
   Serial.print(F("My MAC Address: "));
   Serial.println(WiFi.macAddress());
-  Serial.println(F("ReceiveOK"));
+  Serial.println(F("Waiting for reception"));
 }
 
 
 void loop() {
-  sendF = false;
-
-  //PCからシリアルデータの読み込み
-  while (Serial.available() > 0)  // 受信したデータバッファが1バイト以上存在する場合
+  M5.update();
+  if ( M5.BtnA.wasPressed() )  //ボタンAを押したとき
   {
-    char inChar = (char)Serial.read();  // Serialからデータ読み込み
-
-    if (inChar == '\n')  // 改行コード(LF)がある場合の処理
+    //バッファクリア
+    strData = "";
+    buffp = 0;
+    while (Serial1.available() > 0)
     {
-      baffArr[buffp] = '\0';
-      buffp = 0;
-      sendF = true;
-    }
-    else if (inChar != '\r')  // CRの改行コードの場合は結合しない
-    {
-      baffArr[buffp] = inChar;  // 読み込んだデータを結合
-      if (buffp < DATASIZE)
-      {
-        buffp += 1;
-      }
-      else
-      {
-        selial.println(F("データフォーマット不正。バッファを超えています。"));
-        while (Serial1.available() > 0) {
-          (char)Serial.read();  // Serialデータ読み込み
-        }
-        buffp = 0;
-      }
+      (char)Serial.read();
     }
   }
 
-  //マイコンにLEDの情報を無線送信
-  if (sendF = true)  //データが受信完了した場合に実行
+  //PCからシリアルデータの読み込み
+  while (Serial.available() > 0)       // 受信したデータバッファが1バイト以上存在する場合
   {
-    EspNowSend(&baffArr);  // データESP-NOWで送信
-
-    // 受信したデータの消去
-    for (buffp = 0; buffp < DATASIZE; buffp++) {
-      baffArr[buffp] = '\0';
+    char inChar = (char)Serial.read(); // Serialからデータ読み込み
+    
+    // 改行コード(LF)がある場合の処理
+    if (inChar == '\n')
+    {
+      buffArr[bufp] = '\0';
+      if(buffArr.length() =>25)
+      {
+        //MACアドレス以降の受信した文字列のデータを結合
+        for (int i = 18; i <= bufp; i++)
+          strData += buffArr[bufp]
+        }
+        // データESP-NOWで送信
+        if(EspNowSend(PeerAddress , strData) == 1 ){
+          Serial.println(F("データフォーマット不正。"));
+        }
+      }
+      //バッファクリア
+      strData = "";
+      buffp = 0;
+      while (Serial1.available() > 0)
+      {
+        (char)Serial.read();
+      }
     }
-    buffp = 0;
-    Serial.println(F("ReceiveOK"));
+    else if (inChar != '\r')    // CRの改行コードの場合は結合しない
+    {
+      buffArr[buffp] = inChar; // 読み込んだデータを結合
+      bufp ++;
+      if(buffp == 17) //MACアドレスを配列に変換
+      {
+        char temp[3];
+        uint8_t i;
+        for (i = 0; i < 6; i++) {
+          temp[0] = buffArr[i * 3 + 0];
+          temp[1] = buffArr[i * 3 + 1];
+          temp[2] = 0x00;
+          PeerAddress[i] = strtol(temp, NULL, 16);
+          Serial.printf("PeerAddr[%d] = %d\n", i, PeerAddress[i]);
+        }
+        Serial.println("");
+      }
+      else if (buffp >= DATASIZE)
+      {
+        //バッファクリア
+        strData = "";
+        buffp = 0;
+        while (Serial1.available() > 0)
+        {
+          (char)Serial.read();
+        }
+        selial.println(F("データフォーマット不正。バッファを超えています。"));
+      }
+    }
   }
   delay(1);
 }
